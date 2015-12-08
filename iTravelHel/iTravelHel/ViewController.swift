5fbe7c3700d6caae729fcd4cb844143e3020af57
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-12-08 19:59:06 +0200

    XCode project init

---
+++ iTravelHel/iTravelHel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTravelHel
+//
+//  Created by Jukka-Pekka Seppänen on 8.12.2015.
+//  Copyright © 2015 XCode-Popup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
