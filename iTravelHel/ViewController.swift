52d85a8fc5cea2bdbca625e2805fe3ed0bc1c12a
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-12-11 15:32:25 +0200

    Locationing

--- iTravelHel/ViewController.swift
+++ iTravelHel/ViewController.swift
@@ -13,9 +13,10 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var locateButton: UIButton!
+    var center = CLLocationCoordinate2D()
     
     let locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.locationManager.delegate = self
@@ -25,17 +26,21 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         self.mapView.showsUserLocation = true
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            //Get location
+        let location = locations.last
+        center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
     }
     
     func centerLocation(){
-            //Center map to user location
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        
     }
 }
 
