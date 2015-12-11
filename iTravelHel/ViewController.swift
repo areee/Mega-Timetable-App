c79c0045e5922a0f7b7c5328152924fce61b4792
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-12-11 16:41:32 +0200

    GPS-tracking

--- iTravelHel/ViewController.swift
+++ iTravelHel/ViewController.swift
@@ -12,13 +12,22 @@ import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var locateButton: UIButton!
+    var centerMode = -1;
+    @IBAction func locateMe(sender: AnyObject) {
+        centerLocation()
+        centerMode++
+        if(centerMode > 1){
+            centerMode = -1;
+        }
+    }
+    
     var center = CLLocationCoordinate2D()
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestWhenInUseAuthorization()
@@ -35,12 +44,15 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        if(centerMode == 1){
+            centerLocation()
+        }
     }
     
     func centerLocation(){
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        let zoomLevel = locationManager.location!.horizontalAccuracy * 0.00010
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: zoomLevel, longitudeDelta: zoomLevel))
         self.mapView.setRegion(region, animated: true)
-        
     }
 }
 
